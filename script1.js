3d8cdaedface28fe95f6599b9e8a2c26e396c21d
navdeepawasthy <97968342+navdeepawasthy@users.noreply.github.com>
2022-03-04 14:59:13 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,177 @@
+// Decision making
+
+// one condition
+// if-else
+let a = 10
+if (a==10)
+{
+    //  console.log("success");
+}else
+ {
+    //  console.log("failure")
+}
+// two condition
+// if else ladder
+let a = -10;
+if(a < 0) {
+    console.log("negative")
+}else if {a==o){
+    comnsole.log("zero");
+}else{
+    console.log("positive")
+}
+
+// Three or more Condition
+// switch-case statement
+
+let gender m="a";
+switch(gender){
+     case"m":
+     console.log("male");
+     break;
+     case"f":
+       console.log("female");
+       break;
+       default:
+           console.log("transgender")
+           break;
+} 
+var age=18;
+switch (age){
+    case age<18:
+        console.log("child");
+        break;
+        case 18<age>40:
+        console.log("Adult")
+        break;
+        default:
+            age>40;
+            console.log("old age");
+            break;    
+}
+
+// loops ;;;;
+// iteration statements
+
+// lops-for, while, do while,
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// 3values
+// inintialzation
+// Condition
+// increment/decrement
+
+for(){
+}
+var a;
+var a =12;
+// we  initializing the variable with the value of 12
+
+
+// if there is more than 1 thenif else ladder... will be emplemented
+
+// if else ladder
+
+ let day=10;
+if(day<0){
+    console.log("sunday");
+ }
+else if (monday==1) {
+    console.log("monday");
+} 
+else {
+     console.log("saturday");
+}
+
+//3 or more cond. switch case will be implemented
+
+
+let gender="m";
+switch(gender){
+    case"m":
+    console.log(male);
+    break;
+    // it is important to put break in switch case
+    case"f":
+    console.log(female);
+    break;
+    default:
+    console.log(male);
+    break;
+}
+
+
+// Take age as the variable 
+// age < 18 = Child
+// greater than 18 but less than 40 = Adult 
+// age is greater than 40 = Old age
+
+
+
+let age= 60;
+switch(true){
+      case (age < 18):
+    console.log("child");
+    break;
+    case (18 < age < 40):
+    console.log("adult");
+    break;
+    default:
+    console.log("old age");
+    break;
+}
+
+//ques 2
+// take variable name as day , its possible values are 0 1 2 3 4 5
+// 0 = Monday 
+// 5 : Saturday
+//ans2
+let day=0;
+switch(day){
+    case 0:
+       console.log("sunday");
+        break;
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+         console.log("tuesday");
+         break;
+    case 3:
+         console.log("wednesday");
+         break;
+    case 4:
+         console.log("thursday");
+         break;
+    case 5:
+         console.log("friday");
+         break;
+    default:
+         console.log("saturday");
+         break;
+}
+
+
+
+for (let i = 0; i <= 10; i++) {
+   console.log(i);  
+}
+
+// Question : Print the table of 19 like 19 * 1 = 19
+// 19 * 1 = 19
+// 19 * 2 = 38
+
+
+for (let i = 1; i <= 10; i++) {
+   console.log("19*" + i +"=" + 19*i);
+}
+
